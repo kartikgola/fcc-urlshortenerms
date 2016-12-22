7dbacac31eaf8634a19df0e418c6ff451fa93226
Kartik Gola <Kartik Gola>
2016-12-22 19:54:27 +0530

    Initia. Working on Local

---
+++ routes/shorturl.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var mongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var objectId = require('mongodb').ObjectID;
+
+var dbUrl = "mongodb://127.0.0.1:27017/test";
+
+router.get('/', function (req, res, next) {
+
+    var originalUrl = req.originalUrl.substring(1, req.originalUrl.length);
+    if (originalUrl.search(/[0-9a-z]+/g) == 0) { // If param is Hexadecimal
+        mongoClient.connect(dbUrl, function (err, db) {
+
+            assert.equal(null, err);
+            console.log('Connected correctly to server...');
+            var urlData = db.collection('urlData');
+
+            urlData.findOne({ "shortCode": originalUrl }, function (err, doc) {
+                if (err) {
+                    console.error(err);
+                    return;
+                } else {
+                    if (doc == null) {
+                        res.render('nopage', { message: "Cannot find any URL with that name." });
+                        db.close();
+                    }
+                    else {
+                        console.log('Match found of the short URL submitted...');
+                        console.log('Redirecting...');
+                        res.status(302);
+                        res.redirect(doc.targetUrl);
+                        db.close();
+                    }
+                }
+            }); // findOne
+
+        }); // connect
+    } else { // If bad parameter.
+        console.log('Bad URL...');
+        res.render('nopage', { message: "Cannot find any URL with that name." });
+    }
+}); // router
+
+module.exports = router;
---
+++ routes/url.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var mongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var objectId = require('mongodb').ObjectID;
+
+var dbUrl = "mongodb://127.0.0.1:27017/test";
+
+router.get('/', function (req, res, next) {
+
+  var originalUrl = req.originalUrl.substring(1, req.originalUrl.length);
+  mongoClient.connect(dbUrl, function (err, db) {
+
+    assert.equal(null, err);
+    console.log('Connected correctly to server...');
+    var urlData = db.collection('urlData');
+
+    urlData.count(function (err, num) {
+      console.log(num);
+
+      urlData.insertOne({
+        id: num + 101,
+        shortCode: (num + 101).toString(16),
+        targetUrl: originalUrl
+      }, function (err, result) {
+        if (err)
+          return console.error(err);
+        console.log('Inserted!');
+        res.json({
+          originalUrl : originalUrl,
+          shortUrl : "http://localhost:3000/" + (num+101).toString(16)
+        });
+        db.close();
+      }); // insertOne
+
+    }); // count
+  }); // connect
+
+}); // router
+
+module.exports = router;
